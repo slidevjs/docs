f989a30ece7abe219889add3f3a4cedbd2a19cd3
_Kerman <kermanx@qq.com>
2024-04-12 01:03:14 +0800

    feat: context menu (#1475)
    Co-authored-by: Anthony Fu <anthonyfu117@hotmail.com>

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -158,6 +158,14 @@ const Customizations: (DefaultTheme.NavItemWithLink | DefaultTheme.NavItemChildr
     link: '/custom/config-shortcuts',
   },
   {
+    text: 'Configure Code Runners',
+    link: '/custom/config-code-runners',
+  },
+  {
+    text: 'Configure Context Menu',
+    link: '/custom/config-context-menu',
+  },
+  {
     text: 'Vue Global Context',
     link: '/custom/vue-context',
   },
