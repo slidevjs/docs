3f882f05cd51c554f022b27a61400db9994f1816
Máté Gyöngyösi <gyongyosimate1@gmail.com>
2024-04-15 13:12:14 +0200

    Changed page edit link from the mirrored repo to the actual one (#1534)

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -254,7 +254,7 @@ export default defineConfig({
   themeConfig: {
     logo: '/logo.svg',
     editLink: {
-      pattern: 'https://github.com/slidevjs/docs/edit/main/:path',
+      pattern: 'https://github.com/slidevjs/slidev/edit/main/docs/:path',
       text: 'Suggest changes to this page',
     },
 
